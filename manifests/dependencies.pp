e571fcb0d065dcc6b47be41b139e04e465e609d2
Anthony J Burton <ant@coshx.com>
2011-12-11 18:46:49 -0500

    updates dependencies to handle packages for redhat

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,3 +1,12 @@
 class redis::dependencies {
-  $packages = ["build-essential", "curl"]
+  case $operatingsystem {
+    /(Ubuntu|Debian)/: {
+      $packages = ["build-essential", "curl"]
+      package { $packages: :ensure => installed }
+    }
+    /(Redhat|CentOS|Fedora)/: {
+      $packages = ["gcc", "gcc-c++", "kernel-devel", "curl"]
+      package { $packages: :ensure => installed }
+    }
+  }
 }
